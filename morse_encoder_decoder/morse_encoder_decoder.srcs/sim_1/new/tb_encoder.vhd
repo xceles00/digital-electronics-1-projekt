8d97ea9de3c1ac2d6c3f678c3be6b80526eee169
Čeleš Peter (230540) <xceles00@vutbr.cz>
2023-04-19 16:55:45 +0200

    project update

---
+++ morse_encoder_decoder/morse_encoder_decoder.srcs/sim_1/new/tb_encoder.vhd
@@ -0,0 +1,83 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 04/19/2023 03:53:37 PM
+-- Design Name: 
+-- Module Name: tb_encoder - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity tb_encoder is
+--  Port ( );
+end tb_encoder;
+
+architecture Behavioral of tb_encoder is
+constant c_CLK_100MHZ_PERIOD : time := 10 ns;
+signal sig_rst : std_logic;
+signal sig_SW : std_logic_vector(4 downto 0);
+signal sig_LED : std_logic;
+signal sig_blank : std_logic;
+
+begin
+
+uut_encoder : entity work.encoder
+    port map (
+      SW => sig_SW,
+      rst => sig_rst,
+      LED => sig_LED,
+      blank => sig_blank
+    );
+
+p_reset_gen : process is
+  
+  begin
+
+    sig_rst <= '0';
+    wait for 14 ns;
+    sig_rst <= '1';
+    wait for 28 ns;
+    sig_rst <= '0';
+
+    wait;
+
+  end process p_reset_gen;
+
+p_stimulus : process is
+
+  begin
+    report "Stimulus process started";
+
+    -- WRITE YOUR CODE HERE AND TEST INPUT VALUE
+   
+    sig_SW <= "00001";
+
+    report "Stimulus process finished";
+    wait;
+
+  end process p_stimulus;
+
+end Behavioral;
--- morse_encoder_decoder/morse_encoder_decoder.srcs/sources_1/new/encoder.vhd
+++ morse_encoder_decoder/morse_encoder_decoder.srcs/sources_1/new/encoder.vhd
@@ -20,7 +20,8 @@
 
 
 library IEEE;
-use IEEE.STD_LOGIC_1164.ALL;
+  use ieee.std_logic_1164.all;
+  use ieee.numeric_std.all;
 
 -- Uncomment the following library declaration if using
 -- arithmetic functions with Signed or Unsigned values
@@ -35,27 +36,86 @@ entity encoder is
     Port ( SW : in STD_LOGIC_VECTOR (4 downto 0);
            LED : out STD_LOGIC;
            rst : in STD_LOGIC;
+           clk : in STD_LOGIC;
            blank : in STD_LOGIC);
            
 end encoder;
 
 architecture Behavioral of encoder is
 
+-- Local delay counter
+  signal sig_cnt : unsigned(4 downto 0);
+  signal sig_en : std_logic;
+
+  -- Specific values for local counter
+  constant c_DOT_250ms : unsigned(4 downto 0) := b"1_0000"; --! 4-second delay
+  constant c_DASH_750ms : unsigned(4 downto 0) := b"0_1000"; --! 2-second delay
+  constant c_SPACE_250ms : unsigned(4 downto 0) := b"0_0100"; --! 1-second delay
+
+
 begin
 
+clk_en0 : entity work.clock_enable
+    generic map (
+      -- FOR SIMULATION, KEEP THIS VALUE TO 1
+      -- FOR IMPLEMENTATION, CALCULATE VALUE: 250 ms / (1/100 MHz)
+      -- 1   @ 10 ns
+      -- ??? @ 250 ms
+      g_MAX => 25000000
+    )
+    port map (
+      clk => clk,
+      rst => rst,
+      ce  => sig_en
+    );
+
 p_encoder : process (SW, blank) is
-
-
-
-    if (blank = '1') then
-      LED <= '0';
-      
+    variable morse : string(1 to 4);
+begin
+    if rst = '1' then
+        LED <= '0';
+    elsif blank = '1' then
+        LED <= '0';
     else
+        case SW is
+            when "00001" =>  -- A
+                if (sig_cnt = 0) then
+                    LED <= '1';
+                    sig_cnt <= sig_cnt + 1;
+                else
+                     sig_cnt   <= (others => '0');
+                end if;
+                if (sig_cnt = 1) then
+                    LED <= '0';
+                    sig_cnt <= sig_cnt + 1;
+                else
+                    sig_cnt   <= (others => '0');
+                end if;
+                   if (sig_cnt = 2) then
+                     LED <= '1';
+                        sig_cnt <= sig_cnt + 1;
+                   else
+                        sig_cnt   <= (others => '0');
+                 end if;
+                   if (sig_cnt = 3) then
+                     LED <= '1';
+                        sig_cnt <= sig_cnt + 1;
+                   else
+                        sig_cnt   <= (others => '0');
+                  end if;
+                   if (sig_cnt = 4) then
+                     LED <= '1';
+                        sig_cnt <= sig_cnt + 1;
+                   else
+                        sig_cnt   <= (others => '0');
+                   end if;
+                LED <= '0';
+            when others =>
+                LED <= '1';
+                
+        end case;
 
-      case SW is
-
-        when "00000" =>
-
-          LED <= '1';
+    end if;
+end process;
 
 end Behavioral;
